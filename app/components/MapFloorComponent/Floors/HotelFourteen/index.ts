c7278c495d69463a0e61ba1945414804660841fc
Sesh <sergeimorozov212001@gmail.com>
2025-11-06 20:44:14 +0300

    feat(added all floors)

---
+++ app/components/MapFloorComponent/Floors/HotelFourteen/index.ts
@@ -0,0 +1,8 @@
+import {FirstFloorSvgComponent} from "./FirstFloorSvgComponent";
+import {SecondFloorSvgComponent} from "./SecondFloorSvgComponent";
+import {ThirdFloorSvgComponent} from "./ThirdFloorSvgComponent";
+import {FourthFloorSvgComponent} from "./FourthFloorSvgComponent";
+import {FifthFloorSvgComponent} from "./FifthFloorSvgComponent";
+import {SixthFloorSvgComponent} from "./SixthFloorSvgComponent";
+
+export default { FirstFloorSvgComponent, SecondFloorSvgComponent, ThirdFloorSvgComponent, FourthFloorSvgComponent, FifthFloorSvgComponent, SixthFloorSvgComponent }
