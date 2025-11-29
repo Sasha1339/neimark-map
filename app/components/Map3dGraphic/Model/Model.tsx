998b993994de0a9aea808a216aa012ee92f4a956
Sesh <sergeimorozov212001@gmail.com>
2025-11-29 18:07:16 +0300

    feat(press model)

--- app/components/Map3dGraphic/Model/Model.tsx
+++ app/components/Map3dGraphic/Model/Model.tsx
@@ -5,6 +5,10 @@ import {MeshStandardMaterial} from 'three'
 import {useGLTF} from "@react-three/drei/native";
 import * as THREE from "three";
 
+interface MaterialMesh extends THREE.Mesh {
+  material: THREE.Material | THREE.Material[];
+}
+
 export const Model = () => {
 
 
@@ -16,6 +20,18 @@ export const Model = () => {
 
   let gltfModel: (GLTF & ObjectMap)
 
+  const [selectedObject, setSelectedObject] = useState<THREE.Object3D | null>(null);
+
+  const handleClick = (event: any) => {
+    event.stopPropagation();
+    const object = event.object as MaterialMesh;
+
+    console.log('🎯 КЛИК! Объект:', object.name);
+    // locationContext[1](object ? object.name : null);
+    // console.log(new Date())
+    setSelectedObject(object);
+  };
+
   if (Array.isArray(gltf)) {
     //console.log(gltf);
     gltfModel = gltf[0]
@@ -24,37 +40,45 @@ export const Model = () => {
     gltfModel = gltf;
   }
 
-  const { gl } = useThree();
+  // useEffect(() => {
+  //   if (modelRef.current) {
+  //     modelRef.current.addEventListener('click', handleClick);
+  //   }
+  // }, [modelRef.current])
 
-  const fixAndroidTextures = (scene: any, gl: any) => {
-    scene.traverse((child: any) => {
-      if (child.isMesh && child.material) {
-        const materials = Array.isArray(child.material) ? child.material : [child.material];
+  // const { gl } = useThree();
+  //
+  // const fixAndroidTextures = (scene: any, gl: any) => {
+  //   scene.traverse((child: any) => {
+  //     if (child.isMesh && child.material) {
+  //       const materials = Array.isArray(child.material) ? child.material : [child.material];
+  //
+  //       materials.forEach((material: any) => {
+  //         material.needsUpdate = true;
+  //
+  //         if (material.map) {
+  //           // Устанавливаем правильный colorSpace
+  //           material.map.colorSpace = THREE.SRGBColorSpace;
+  //           material.map.flipY = false;
+  //           material.map.needsUpdate = true;
+  //
+  //           // Принудительно инициализируем текстуру в WebGL
+  //           gl.initTexture(material.map);
+  //         }
+  //       });
+  //     }
+  //   });
+  // };
 
-        materials.forEach((material: any) => {
-          material.needsUpdate = true;
 
-          if (material.map) {
-            // Устанавливаем правильный colorSpace
-            material.map.colorSpace = THREE.SRGBColorSpace;
-            material.map.flipY = false;
-            material.map.needsUpdate = true;
+  // useEffect(() => {
+  //   if (gltfModel && gltfModel.scene) {
+  //     fixAndroidTextures(gltfModel.scene, gl);
+  //   }
+  // }, [gltfModel, gl]);
 
-            // Принудительно инициализируем текстуру в WebGL
-            gl.initTexture(material.map);
-          }
-        });
-      }
-    });
-  };
-
-
-  useEffect(() => {
-    if (gltfModel && gltfModel.scene) {
-      fixAndroidTextures(gltfModel.scene, gl);
-    }
-  }, [gltfModel, gl]);
-
-  return <primitive ref={modelRef} object={gltfModel.scene} />;
+  return <primitive ref={modelRef} object={gltfModel.scene} onPointerDown={handleClick} onDoubleClick={() => {
+    console.log('Pointer up!');
+  }} />;
 
 }
