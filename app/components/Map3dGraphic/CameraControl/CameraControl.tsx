145182328fc1e8fc05f33da08605200c699a0992
Sesh <sergeimorozov212001@gmail.com>
2025-12-01 17:07:16 +0300

    feat(buildings version 1)

--- app/components/Map3dGraphic/CameraControl/CameraControl.tsx
+++ app/components/Map3dGraphic/CameraControl/CameraControl.tsx
@@ -21,14 +21,11 @@ export const CameraControl: FC<Props> = ({OrbitControls}) => {
   const objectsContext = useContext(MapObjectsContext);
 
 
-  // const target = useRef<THREE.Vector3>([0, 5, 0]);
-
   const [target, setTarget] = useState<THREE.Vector3>(new THREE.Vector3(0, 5, 0));
 
-  // const panEnabled = useRef(true);
 
   const building = useRef(false)
-  // const rotationAngle = useRef(0)
+
 
   useFrame((state, delta, frame) => {
 
@@ -39,21 +36,17 @@ export const CameraControl: FC<Props> = ({OrbitControls}) => {
 
       building.current = true;
 
-      // ПАРАМЕТРЫ ВРАЩЕНИЯ
-      const ROTATION_SPEED = 0.5; // радиан в секунду (настройте под себя)
-      const ROTATION_RADIUS = 1;  // радиус вращения
-      const ROTATION_HEIGHT = 1.5;  // высота камеры
 
-      // 1. Увеличиваем угол вращения с учетом времени между кадрами
-      // rotationAngle.current += ROTATION_SPEED * delta;
-      //
-      // 2. Рассчитываем позицию камеры на круговой орбите
+      const ROTATION_RADIUS = 1;
+      const ROTATION_HEIGHT = 1.5;
+
+
       if (objectsContext.rotationAngleRef.current !== null) {
         const cameraX = position.x + Math.cos(objectsContext.rotationAngleRef.current) * ROTATION_RADIUS;
         const cameraY = position.y + ROTATION_HEIGHT;
         const cameraZ = position.z + Math.sin(objectsContext.rotationAngleRef.current) * ROTATION_RADIUS;
 
-        // 3. Плавно двигаем камеру к этой позиции
+
         camera.position.lerp(
           new THREE.Vector3(cameraX, cameraY, cameraZ),
           stepOn
@@ -62,17 +55,12 @@ export const CameraControl: FC<Props> = ({OrbitControls}) => {
 
 
 
-      // 4. Смотрим на объект
       camera.lookAt(position.x, position.y, position.z);
 
-      // }
 
-
-      // camera.updateProjectionMatrix()
 
     } else if (building.current ) {
-      // console.log(camera.current.rotation);
-      // const matrix = new THREE.Matrix4().setPosition( new THREE.Vector3(0, 0, 0));
+
       camera.position.set(0, 5, 0);
 
       camera.position.applyEuler(new THREE.Euler(0, 0, Math.PI, "XYZ"))
@@ -84,16 +72,7 @@ export const CameraControl: FC<Props> = ({OrbitControls}) => {
 
   return (
     <OrbitControls
-      onChange={(e) => {
-        // const rotation = e.target.camera?.rotation
-        // if (rotation && firstEnter.current) {
-        //   e.target.camera?.rotation.set(rotation.x, rotation.y, rotation.z + Math.PI / 3);
-        //   firstEnter.current = false;
-        // } else if (rotation) {
-        //   e.target.camera?.rotation.set(rotation.x, rotation.y, rotation.z);
-        // }
-      }}
-      enabled={!building.current}
+      enabled={true}
       enableZoom={true}
       enableRotate={true}
       minPolarAngle={0}
